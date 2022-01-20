43dc9a7f78eb613ef31e8f8526bdf208a57f28d2
josecarlos munoz <josecarlos.munoz12@outlook.com>
2022-01-19 18:25:33 -0600

    version check

--- Test/main.py
+++ Test/main.py
@@ -2,6 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import sys
 
 
 def print_hi(name):
@@ -12,5 +13,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print(sys.version)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
