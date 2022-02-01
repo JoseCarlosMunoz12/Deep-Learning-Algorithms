6286718096aeb5c74f4db67c16d5f107c6bfb565
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2022-02-01 15:01:23 -0600

    Simple functions Of Tensor Flow

--- Week 3/Programming/main.py
+++ Week 3/Programming/main.py
@@ -6,3 +6,28 @@ d = tf.zeros((2, 2))
 e = tf.ones((2, 2))
 print(d)
 print(e)
+
+a1 = tf.constant(5)
+b1 = tf.constant(2)
+c1 = tf.add(a1, b1, name='Add')
+print(c1)
+
+print(d)
+print(tf.reshape(d, (1, 4)))
+a1 = a1.numpy()
+a2 = d.numpy()
+print(a1, '\n', a2)
+
+tens_3d = tf.constant([[[1, 2], [3, 4]], [[5, 6], [7, 8]]], dtype=float)
+print(tens_3d[1, 1, 0])
+
+real = tf.constant([2.25, 3.25])
+imag = tf.constant([4.75, 5.75])
+y = tf.complex( real, imag)
+print(y)
+
+b = tf.constant([1, 2])
+Al = tf.constant([[2, 24], [2, 26], [2, 57]])
+x = [[1, 2, 3]]
+Cl = tf.matmul(x, Al) + b
+print(Cl)
