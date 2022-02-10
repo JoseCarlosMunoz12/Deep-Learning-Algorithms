dca100211f9d21e84746f8d154a242fc5aa830d5
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2022-02-10 14:53:20 -0600

    Need to fix keras loading

--- Week 4/Programming 3/MyModule.py
+++ Week 4/Programming 3/MyModule.py
@@ -19,3 +19,37 @@ class MyModel(tf.Module):
             if b is not None:
                 self._b_variable.assign(b)
         return tf.matmul(self._w_variable, x) + self._b_variable
+
+
+class NewModel(tf.Module):
+    def __init__(self, name=None):
+        super().__init__(name=name)
+        self._layer_1 = MyModel()
+        self._layer_2 = MyModel()
+
+    def __call__(self, x, w1=None, b1=None, w2=None, b2=None):
+        if w1 is None:
+            y_1 = self._layer_2(x, w2, b2)
+            return y_1
+        else:
+            y_2 = self._layer_1(x, w1, b1)
+            y_2 = self._layer_2(y_2, w2, b2)
+            return y_2
+
+
+class Linear(keras.layers.Layer):
+    def __init__(self, units=32, input_dim=32):
+        super(Linear, self).__init()
+        w_init = tf.random_normal_initializer()
+        self.__w = tf.Variable(
+            initial_value=w_init(shape=(input_dim, units), dtype="float32"),
+            trainable=True
+        )
+        b_init = tf.zeros_initializer()
+        self.__b = tf.Variable(
+            initial_value=w_init(shape=(units,), dtype="float32"),
+            trainable=True
+        )
+
+    def __call__(self, inputs):
+        return tf.matmul(inputs, self.__w) + self.__b
--- Week 4/Programming 3/main.py
+++ Week 4/Programming 3/main.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 from MyModule import MyModel as Mm
-
+from MyModule import Linear
 model = Mm()
 w_0 = tf.constant([[1.0, 2.0], [2.0, 3.0], [3.0, 4.0]])
 b_0 = tf.constant( [[1.0], [2.0], [1.0]])
@@ -13,3 +13,8 @@ p = model(x, w_0, b_0).numpy()
 print("Forward computation", p)
 print("trainable variables", model.trainable_variables)
 print("all variables", model.variables)
+
+x = tf.ones((2, 2))
+lin = Linear(4, 2)
+y = lin(x)
+print(y)
