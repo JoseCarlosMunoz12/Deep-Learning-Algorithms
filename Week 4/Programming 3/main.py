d9c1f1fc6c6e071a0c372a72588fe8d50b36b65e
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2022-02-18 21:13:19 -0600

    Testing Keras

--- Week 4/Programming 3/main.py
+++ Week 4/Programming 3/main.py
@@ -1,19 +1,52 @@
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
-from MyModule import MyModel as Mm
-from MyModule import Linear
+from keras.datasets import imdb
+from keras import models
+from keras import layers
 
-model = Mm()
-w_0 = tf.constant([[1.0, 2.0], [2.0, 3.0], [3.0, 4.0]])
-b_0 = tf.constant( [[1.0], [2.0], [1.0]])
-x = [[1.0], [1.0]]
-p = model(x, w_0, b_0).numpy()
-print("Forward computation", p)
-print("trainable variables", model.trainable_variables)
-print("all variables", model.variables)
 
-x = tf.ones((2, 2))
-lin = Linear(4, 2)
-y = lin(x)
-print(y)
+def vectorize_sequences(sequences, dimension=10000):
+    # Create an all-zero matrix of shape (len(sequences), dimension)
+    results = np.zeros((len(sequences), dimension))
+    for i, sequence in enumerate(sequences):
+        results[i, sequence] = 1.  # set specific indices of results[i] to 1s
+    return results
+
+
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+word_index = imdb.get_word_index()
+reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
+print(train_data[0], '\n')
+decoded_review = ' '.join([reverse_word_index.get(i - 3, '\n') for i in train_data[0]])
+print(decoded_review)
+print('the label of this review is: ', train_labels[0])
+
+imdb_model = models.Sequential()
+imdb_model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
+imdb_model.add(layers.Dense(16, activation='relu'))
+imdb_model.add(layers.Dense(1, activation='sigmoid'))
+imdb_model.compile(optimizer='rmsprop',
+                   loss='binary_crossentropy',
+                   metrics=['accuracy'])
+x_train = vectorize_sequences(train_data)
+# Our vectorized test data
+x_test = vectorize_sequences(test_data)
+print(x_train[0])
+# Our vectorized labels
+y_train = np.asarray(train_labels).astype('float32')
+y_test = np.asarray(test_labels).astype('float32')
+
+x_val = x_train[:10000]
+partial_x_train = x_train[10000:]
+
+y_val = y_train[:10000]
+partial_y_train = y_train[10000:]
+
+history = imdb_model.fit(partial_x_train,
+                         partial_y_train,
+                         epochs=20,
+                         batch_size=512,
+                         validation_data=(x_val, y_val))
+history_dict = history.history
+history_dict.keys()
