bcdf1c3523078d177a6758816c76a3f34c0cc8a6
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2022-02-19 21:22:50 -0600

    Added matplot
    Need to swith the python 3.9 to 3.7 to get matplotlib working with tensorflow

--- Week 4/Programming 3/main.py
+++ Week 4/Programming 3/main.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from tensorflow import keras
+import matplotlib.pyplot as plt
 import numpy as np
 from keras.datasets import imdb
 from keras import models
@@ -50,3 +51,19 @@ history = imdb_model.fit(partial_x_train,
                          validation_data=(x_val, y_val))
 history_dict = history.history
 history_dict.keys()
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+# "bo" is for "blue dot"
+plt.plot(epochs, loss, 'bo', label='Training loss')
+# b is for "solid blue line"
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
