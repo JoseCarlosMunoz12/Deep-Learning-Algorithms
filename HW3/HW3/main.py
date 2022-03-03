10524eb6b4f5d502fdff5d0b29401e685d984a5e
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2022-03-02 22:03:29 -0600

    trying to fix format of data

--- HW3/HW3/main.py
+++ HW3/HW3/main.py
@@ -4,6 +4,7 @@ from keras import layers
 import numpy as np
 import math
 import os
+import pandas as pd
 from numpy.random import default_rng
 import matplotlib.pyplot as plt
 
@@ -35,7 +36,7 @@ def get_file(fileName=""):
         fName = fileName
     file = os.path.join(base_dir, fName)
     try:
-        data = open(file, 'r')
+        data = pd.read_csv(file)
         ferror = False
     except FileNotFoundError:
         ferror = True
@@ -51,66 +52,36 @@ def get_file(fileName=""):
                     exit
                 else:
                     try:
-                        data = open(fName1, 'r')
+                        data = pd.read_csv(fName1)
                         rep = False
                     except:
                         print('incorrect file name')
     return data
 
 
-def separate_data(lines):
-    dic = {}
-    data = []  # holds all of feature for each point
-    labels = []  # holds all of the labels for each point
-    # Parsing data
-    for line in lines:
-        newstr = line.strip()
-        parsed = newstr.split(',')
-        if not (parsed[-1]) in dic:
-            dic[parsed[-1]] = len(dic)
-        temp = []
-        for item in parsed[:-1]:
-            temp.append(mk_float(item))
-        data.append(temp)
-        labels.append(dic[parsed[-1]])
-    # Seperate dataand labels to training, validation and testing set
-    cols = len(data[1])
-    rows = len(data)
+def separate_data(data):
+    print(data)
+    cols = data.shape[1]
+    rows = data.shape[0]
+    X = np.array(data.iloc[:, 0:cols - 2], np.float32)
+    y = np.array(data.iloc[:, cols - 2:cols - 1], np.float32)
     rng = default_rng()
-    # Select training and testing data
+    # select training and testing data
     train_idx = rng.choice(rows, size=math.ceil(rows * 3 / 4), replace=False)
+    x_train = X[train_idx, :]
+    y_train = y[train_idx, :]
     test_idx = np.setdiff1d(np.array(range(0, rows)), train_idx)
-    x_test = []
-    y_test = []
-    for test in test_idx:
-        x_test.append(data[test])
-        y_test.append(labels[test])
-    x_train = []
-    y_train = []
-    for train in train_idx:
-        x_train.append(data[train])
-        y_train.append(labels[train])
-    # separate training set to partial training and validation set
-    partial_test_set = rng.choice(len(x_train), size=math.ceil((len(x_train)) * 8 / 10), replace=False)
-    valid_set = np.setdiff1d(np.array(range(0, len(x_train))), partial_test_set)
-    x_p_train = []
-    y_p_train = []
-    for p_train in partial_test_set:
-        x_p_train.append(data[p_train])
-        y_p_train.append((labels[p_train]))
-    x_validation = []
-    y_validation = []
-    for validation in valid_set:
-        x_validation.append(data[validation])
-        y_validation.append(labels[validation])
-    return (x_test, x_p_train, x_validation), (y_test, y_p_train, y_validation)
+    x_test = X[test_idx, :]
+    y_test = y[test_idx, :]
+    return
 
 
 def main():
     # Loading file
-    file = get_file('house.txt')
-    lines = file.readlines()
-    (x_test, x_pTrain, x_valid), (y_test, y_pTrain, y_valid) = separate_data(lines)
+    data = get_file('house.csv')
+    separate_data(data)
+    # (x_test, x_pTrain, x_valid), (y_test, y_pTrain, y_valid) =
+    exit()
     # Set results as categorical
     y_test = tf.keras.utils.to_categorical(y_test)
     y_pTrain = tf.keras.utils.to_categorical(y_pTrain)
@@ -118,7 +89,7 @@ def main():
     x_dimen = len(x_test[0])
     # Create ANN network
     house_model = models.Sequential()
-    house_model.add(layers.Dense(64, activation='relu', input_shape=x_dimen))
+    house_model.add(layers.Dense(64, activation='relu', input_shape=(x_dimen,)))
     house_model.add(layers.Dense(32, activation='relu'))
     house_model.add(layers.Dense(16, activation='relu'))
     house_model.add(layers.Dense(8, activation='relu'))
