56fbdf0d8489ed8b96cf69582d38fbdddd739346
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2022-03-31 13:43:57 -0500

    Started test of RNN
    started RNN testing and learning

---
+++ Week 11/RNN/main.py
@@ -0,0 +1,24 @@
+import tensorflow as tf
+from pandas import read_csv
+from datetime import datetime
+import os
+import matplotlib.pyplot as plt
+from pandas import DataFrame
+from pandas import concat
+from keras import models
+from keras import layers
+from keras import optimizers
+
+try:
+    from google.colab import drive
+    IN_COLAB=True
+except:
+    IN_COLAB=False
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
