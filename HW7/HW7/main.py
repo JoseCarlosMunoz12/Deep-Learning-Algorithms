4924ca3846e2d63b29474acd9400d273839efa15
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2022-04-12 13:43:44 -0500

    Able to parse data
    Able to parse data, encode it and minMax it

--- HW7/HW7/main.py
+++ HW7/HW7/main.py
@@ -22,6 +22,20 @@ except:
     IN_COLAB = False
 
 
+def plot_results(history):
+    plt.close()
+    plt.plot(history.history['mse'], 'r',label='MSE')
+    plt.plot(history.history['val_mse'], 'b', label='Validation MSE')
+    plt.title('Training and validation Mean Squared Error')
+    plt.legend()
+    plt.figure()
+    plt.plot(history.history['loss'], label='train')
+    plt.plot(history.history['val_loss'], label='valid')
+    plt.title('Training and validation Loss (MAE)')
+    plt.legend()
+    plt.show()
+
+
 def parse(x):
     return datetime.strptime(x, '%Y %m %d %H')
 
@@ -58,10 +72,95 @@ def loadfile():
     return dataset
 
 
+def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
+    n_vars = 1 if type(data) is list else data.shape[1]
+    df = DataFrame(data)
+    cols, names = list(), list()
+    for i in range(n_in, 0, -1):
+        cols.append(df.shift(i))
+        names += [('var%d(t-%d)' % (j + 1, i)) for j in range(n_vars)]
+    for i in range(0, n_out):
+        cols.append(df.shift(-i))
+        if i == 0:
+            names += [('var%d(t)' % (j+1)) for j in range(n_vars)]
+        else:
+            names += [('var%d(t+%d)' % (j+1, i)) for j in range(n_vars)]
+    agg = concat(cols, axis=1)
+    agg.columns = names
+    if dropnan:
+        agg.dropna(inplace=True)
+    return agg
+
+
+def normalize_reformat_prepare(dataset, n_in=1, n_out=1):
+    values = dataset.values
+    encoder = LabelEncoder()
+    values[:, 10] = encoder.fit_transform(values[:, 10])
+    values[:, 12] = encoder.fit_transform(values[:, 12])
+    values = values.astype('float32')
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    scaled = scaler.fit_transform(values)
+    reframed = series_to_supervised(scaled, n_in, n_out)
+    reframed.drop(reframed.columns[[9, 10, 11, 12, 13, 14, 15]], axis=1, inplace=True)
+    print(reframed.head())
+    return reframed, scaler
+
+
+def train_model(dataset, title, n_in=1, n_out=1):
+    reframed, scaler = normalize_reformat_prepare(dataset, n_in, n_out)
+    # prepare data
+    values = reframed.values
+    n_train_hours = int(round(len(values)/3))
+    n_valid_hours = int(round(2 * len(values)/3))
+    train = values[: n_train_hours, :]
+    valid = values[n_train_hours:n_valid_hours, :]
+    test = values[n_valid_hours:, :]
+    # split dat into training input, validation output and later testing input
+    train_x, train_y = train[:, :-1], train[:, -1]
+    valid_x, valid_y = valid[:, :-1], valid[:, -1]
+    test_x, test_y = test[:, :-1], test[:, -1]
+    # reshape input to be 3D[samples, timesteps, features]
+    train_x = train_x.reshape((train_x.shape[0], 1, train_x.shape[1]))
+    valid_x = valid_x.reshape((valid_x.shape[0], 1, valid_x.shape[1]))
+    test_x = test_x.reshape((test_x.shape[0], 1, test_x.shape[1]))
+    # Display shape of models
+    print(train_x.shape, train_y.shape)
+    print(valid_x.shape, valid_y.shape)
+    print(test_x.shape, test_y.shape)
+    # make model
+    print(train_x.shape[0])
+    print(train_x.shape[1])
+    print(train_x.shape[2])
+    model = models.Sequential()
+    model.add(layers.LSTM(50, input_shape=(train_x.shape[1], train_x.shape[2])))
+    model.add(layers.Dense(1))
+    model.compile(loss='mae', optimizer='adam', metrics=['mse'])
+    # fit network
+    history = model.fit(train_x, train_y, epochs=50, batch_size=96, validation_data=(valid_x, valid_y), verbose=2,
+                        shuffle=False)
+    plot_results(history)
+    # seeing results of the data
+    y_hat = model.predict(test_x)
+    rmse0 = sqrt(mean_squared_error(test_y,y_hat))
+    test_x = test_x.reshape((test_x.shape[0], test_x.shape[2]))
+    # invert scaling for forecast
+    inv_y_hat = concatenate((y_hat, test_x[:, 1:]), axis=1)
+    inv_y_hat = scaler.inverse_transform(inv_y_hat)
+    inv_y_hat = inv_y_hat[:, 0]
+    # invert scaling for actual
+    test_y = test_y.reshape((len(test_y), 1))
+    inv_y = concatenate((test_y, test_x[:, 1:]), axis=1)
+    inv_y = scaler.inverse_transform(inv_y)
+    inv_y = inv_y[:, 0]
+    rmse = sqrt(mean_squared_error(inv_y, inv_y_hat))
+    print('Test RMSE scaled: %.3f' % rmse0)
+    print('Test RMSE absolute: %.3f' % rmse)
+
+
 def main():
     # Loading file
     dataset = loadfile()
-    print(dataset)
+    train_model(dataset, 'Two Day Prediction', 1, 1)
 
 
 if __name__ == '__main__':
